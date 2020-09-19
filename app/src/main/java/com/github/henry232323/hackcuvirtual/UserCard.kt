5582b8c5639304e614f38078547799ceba650970
Zach Elkins <zach.elkins@gmail.com>
2020-09-19 15:05:59 -0600

    Added basic UserCard Class. Working on implementation.

--- app/src/main/java/com/github/henry232323/hackcuvirtual/UserCard.kt
+++ app/src/main/java/com/github/henry232323/hackcuvirtual/UserCard.kt
@@ -12,3 +12,45 @@ class UserCard( username: String, displayName: String, gender: String, favorites
         println("Username: $uName\n DisplayName: $dName")
     }
 }
+
+
+/*
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val populateButtonHS: Button = findViewById(R.id.populateButtonHS)
+        val populateButtonEB: Button = findViewById(R.id.populateButtonEB)
+        val populateButtonZE: Button = findViewById(R.id.populateButtonZE)
+
+        val hs = UserCard( "willemhscott", "Henry Scott", "Male", listOf("Men"), listOf("Apples"), true )
+        val eb = UserCard( "Bico", "Enri-C-not-QU-o", "Male", listOf("Ham"), listOf("Vegatbles"), false )
+        val ze = UserCard( "zelkins18", "Zach Elkins", "Male", listOf("Sushi", "Grapes"), listOf("Peanuts"), false )
+
+        populateButtonHS.setOnClickListener { populateData( hs ) }
+        populateButtonEB.setOnClickListener { populateData( eb ) }
+        populateButtonZE.setOnClickListener { populateData( ze ) }
+    }
+
+    private fun populateData( user: UserCard ) {
+
+        val username: TextView = findViewById(R.id.username)
+        val displayname: TextView = findViewById(R.id.displayname)
+        val gender: TextView = findViewById(R.id.gender)
+        val favorites: TextView = findViewById(R.id.favorites)
+        val allergies: TextView = findViewById(R.id.allergies)
+        val covid: TextView = findViewById(R.id.covid)
+
+        username.setText(user.uName)
+        displayname.setText(user.dName)
+        gender.setText(user.gender)
+        favorites.setText(user.favs.toString())
+        allergies.setText(user.alrgs.toString())
+        covid.setText(user.cov19.toString())
+
+    }
+}
+*
+* */
