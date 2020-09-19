e28abaabd08c6edda0d97d3c975b401900cfb9c3
Zach Elkins <zach.elkins@gmail.com>
2020-09-19 15:55:20 -0600

    Added new activities

--- app/src/main/java/com/github/henry232323/hackcuvirtual/MatchesActivity.kt
+++ app/src/main/java/com/github/henry232323/hackcuvirtual/MatchesActivity.kt
@@ -3,9 +3,9 @@ package com.github.henry232323.hackcuvirtual
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class MatchesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_matches)
     }
 }
