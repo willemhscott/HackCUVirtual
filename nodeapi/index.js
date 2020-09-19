9c4a220140c4b70285aa18636b946f37d227f216
Enrico Blackwell <pumpkintitan11@gmail.com>
2020-09-18 19:45:10 -0600

    test change

--- nodeapi/index.js
+++ nodeapi/index.js
@@ -20,6 +20,7 @@ const client = new Client({
 			console.log('connected');
 		}
 	});
+	console.log('connected to ' + client.database);
 	// const res = await client.query('SELECT $1::text as message', [ 'Hello world!' ]);
 	// console.log(res.rows[0].message); // Hello world!
 	await client.end();
