0b7426bcdab3a7df4bca4b4aa669c4eefc64edd4
willemhscott <willem.scott@colorado.edu>
2020-09-19 13:32:07 -0600

    Some messaging stuff

--- app/src/main/java/com/github/henry232323/hackcuvirtual/Messenger.kt
+++ app/src/main/java/com/github/henry232323/hackcuvirtual/Messenger.kt
@@ -1,42 +1,73 @@
 package com.github.henry232323.hackcuvirtual
 
-import android.net.http.HttpResponseCache.install
 import android.os.Bundle
 import android.os.PersistableBundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+import com.tinder.scarlet.Lifecycle
+import com.tinder.scarlet.MessageAdapter
 import com.tinder.scarlet.Scarlet
 import com.tinder.scarlet.WebSocket
+import com.tinder.scarlet.lifecycle.android.AndroidLifecycle
+import com.tinder.scarlet.messageadapter.moshi.MoshiMessageAdapter
+import com.tinder.scarlet.retry.ExponentialWithJitterBackoffStrategy
+import com.tinder.scarlet.streamadapter.rxjava2.RxJava2StreamAdapterFactory
 import com.tinder.scarlet.ws.Receive
 import com.tinder.scarlet.ws.Send
-import java.net.URI;
-import java.net.URISyntaxException;
 import io.reactivex.Flowable
+import java.sql.Timestamp
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 
+data class Authentication (
+    val type: String = "authenticate",
+    val username: String,
+    val password: String
+)
+
+data class Message (
+    val type: String = "message",
+    val from: String,
+    val to: String,
+    val timestamp: Long = System.currentTimeMillis(),
+    val content: String
+)
 
 interface WebsocketClient {
+    @Send
+    fun sendAuth(auth: Authentication)
+    @Send
+    fun sendMessage(message: Message)
+    @Receive
+    fun observeMessage(): Flowable<Message>
+    @Receive
+    fun observeOnConnectionOpenedEvent(): Flowable<WebSocket.Event.OnConnectionOpened<*>>
     @Receive
     fun observeWebSocketEvent(): Flowable<WebSocket.Event>
-    @Send
-    fun sendSubscribe(subscribe: Subscribe)
-    @Receive
-    fun observeTicker(): Flowable<Ticker>
 }
 
+const val WEBSOCK_URL = "wss://3.17.77.33"
 class Messenger : AppCompatActivity()  {
+
+    fun createAppForegroundAndUserLoggedInLifecycle(): Lifecycle {
+        return AndroidLifecycle.ofApplicationForeground(application)
+            .combineWith(loggedInLifecycle)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        val scarletInstance = Scarlet.Builder()
-            .webSocketFactory(okHttpClient.newWebSocketFactory("wss://ws-feed.gdax.com"))
+        val BACKOFF_STRATEGY = ExponentialWithJitterBackoffStrategy(1000, 60000)
+        val scarletInst = Scarlet.Builder()
+            .backoffStrategy(BACKOFF_STRATEGY)
             .addMessageAdapterFactory(MoshiMessageAdapter.Factory())
             .addStreamAdapterFactory(RxJava2StreamAdapterFactory())
+            .lifecycle(createAppForegroundAndUserLoggedInLifecycle())
             .build()
 
-        val service = scarletInstance.create<WebsocketClient>()
+
+        val scarletMessenger = scarletInst.create<Messenger>()
+
+
+        scarletMessenger.sendMes
     }
     /*
 
