aef39266a47b85b0de5e17b1d5cb25564ae62fde
willemhscott <willem.scott@colorado.edu>
2020-09-20 12:15:46 -0600

    fix crash

--- app/src/main/java/com/github/henry232323/hackcuvirtual/UserViewActivity.kt
+++ app/src/main/java/com/github/henry232323/hackcuvirtual/UserViewActivity.kt
@@ -1,20 +1,18 @@
 package com.github.henry232323.hackcuvirtual
 
-import android.graphics.BitmapFactory
-import android.net.Uri
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import org.json.JSONArray
 import org.json.JSONObject
 
-import com.bumptech.glide.Glide
-
 
 class UserViewActivity : AppCompatActivity() {
-    var counter = 0;
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,34 +21,41 @@ class UserViewActivity : AppCompatActivity() {
         val likeButton: Button = findViewById(R.id.btnLike)
         val dislikeButton: Button = findViewById(R.id.btnDislike)
 
-        getUserData( LoginActivity.current_user )
+        getUserData(LoginActivity.current_user)
 
         likeButton.setOnClickListener {
-            getUserData( LoginActivity.current_user )
+            getUserData(LoginActivity.current_user)
         }
 
         dislikeButton.setOnClickListener {
-            getUserData( LoginActivity.current_user )
+            getUserData(LoginActivity.current_user)
         }
 
     }
 
-    fun getUserData(username:String) {
-        Messenger.instance.getPotentialMatches(username, { arr: JSONArray ->
-            loadProfile( arr[counter] as JSONObject )
-            counter += 1;
-        } )
+    fun getUserData(username: String) {
+        Messenger.instance.getPotentialMatches(username) { arr: JSONArray ->
+            if (counter == arr.length()) {
+                counter = 0
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                loadProfile(arr[counter] as JSONObject)
+                counter += 1
+            }
+        }
     }
 
-    fun loadProfile( data: JSONObject ) {
-        val user = UserCard( data.getString("username"),
+    fun loadProfile(data: JSONObject) {
+        val user = UserCard(
+            data.getString("username"),
             data.getString("display_name"),
             data.getInt("age"),
             data.getString("gender"),
             data.getJSONArray("favorites"),
-            data.getJSONArray( "allergens"),
+            data.getJSONArray("allergens"),
 //            data.getBoolean("covid"))//,
-            data.getString("photo") )
+            data.getString("photo")
+        )
 
         val displayName: TextView = findViewById(R.id.name_display)
         val displayAge: TextView = findViewById(R.id.age_display)
@@ -59,12 +64,15 @@ class UserViewActivity : AppCompatActivity() {
         val displayAllergies: TextView = findViewById(R.id.allergen_display)
         val displayImage: ImageView = findViewById(R.id.imageView)
 
-        displayName.setText(user.dName)
-        displayAge.setText(user.age.toString())
-        displayGender.setText(user.gender)
-        displayFavorites.setText(user.getFavorites())
-        displayAllergies.setText(user.getAllergies())
-        Glide.with(this).load(user.photo).into(displayImage)
+        displayName.text = user.dName
+        displayAge.text = user.age.toString()
+        displayGender.text = user.gender
+        displayFavorites.text = user.getFavorites()
+        displayAllergies.text = user.getAllergies()
+        runOnUiThread {
+            Glide.with(this).load(user.photo).into(displayImage)
+        }
+
 //        displayImage.setImageBitmap(bmp)
     }
 
