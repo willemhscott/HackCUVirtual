ddeb64a422dba82aedf81dfdb76165400a5b2fe9
willemhscott <willem.scott@colorado.edu>
2020-09-19 20:32:35 -0600

    dsadas

--- nodeapi/index.js
+++ nodeapi/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const app = express();
-const port = 3000;
+const port = 80;
 
 const { Pool } = require('pg');
 
